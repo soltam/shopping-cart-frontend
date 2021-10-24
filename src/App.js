562768adae25a4830a89d577f03f8e382fa992ae
Gautam Solaimalai <gautam.solaimalai@honeywell.com>
2021-10-24 15:50:19 -0400

    Added material UI and styles, Added basic structure of the code, Added fonts, Added Shopping Cart.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppHeader from "./Components/AppHeader/AppHeader";
+import ShoppingList from "./Pages/ShoppingList/ShoppingList";
+import Box from "@mui/material/Box";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppHeader />
       </header>
-    </div>
+      <main>
+        <Box className="App-content">
+          <ShoppingList />
+        </Box>
+      </main>
+    </>
   );
 }
 
---
+++ src/Components/AppHeader/AppHeader.jsx
@@ -0,0 +1,13 @@
+import './AppHeader.scss';
+
+const AppHeader = () => {
+    return (
+        <div className="header">
+            <span className="headerTitle">
+                SHOPPING LIST
+            </span>
+        </div>
+    );
+}
+
+export default AppHeader;
